6f91ae38e3c2ab84f74485f2d41782938ef94aac
Lightshadow - Laptop <lightshadowman@gmail.com>
2018-06-02 20:37:45 -0400

    google ads test 2

--- partials/footers.php
+++ partials/footers.php
@@ -1,14 +1,4 @@
-<!-- SE Google Ads-->
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- SE -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-5215030892730474"
-     data-ad-slot="2669137948"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+
 
 <!-- Google Analytics Script (Global site tag (gtag.js)) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115409161-1"></script>
@@ -34,5 +24,17 @@
 
 <footer>
 	<p>Dulcet Games Inc. © 2016 - 2018. All rights reserved.</p><br>
-	<p class="footer_link"><a href="index.php">HOME</a> | <a href="terms.php">TERMS</a> | <a href="shop.php">SHOP</a> | <a href="about.php">ABOUT</a> | <a href="contact.php">CONTACT</a> | <a href="parents.php">FOR PARENTS</a></p>
+    <p class="footer_link"><a href="index.php">HOME</a> | <a href="terms.php">TERMS</a> | <a href="shop.php">SHOP</a> | <a href="about.php">ABOUT</a> | <a href="contact.php">CONTACT</a> | <a href="parents.php">FOR PARENTS</a></p>
+    
+    <!-- SE Google Ads-->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- SE -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-5215030892730474"
+        data-ad-slot="2669137948"
+        data-ad-format="auto"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </footer>
