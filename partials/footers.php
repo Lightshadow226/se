05c11e077cc7895c56682f55654095067704d032
Lightshadow - Laptop <lightshadowman@gmail.com>
2018-06-02 20:35:20 -0400

    google ads test

--- partials/footers.php
+++ partials/footers.php
@@ -1,4 +1,26 @@
 <!-- SE Google Ads-->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- SE -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5215030892730474"
+     data-ad-slot="2669137948"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<!-- Google Analytics Script (Global site tag (gtag.js)) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115409161-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-115409161-1');//Analytics
+</script>
+
+<!-- 
 <ins class="adsbygoogle googleAds"
     style="background-color: white;"
     data-ad-client="ca-pub-5215030892730474"
@@ -6,7 +28,7 @@
     data-ad-slot="4063247438"
     data-ad-format="auto">
 </ins>
-
+-->
 <!-- Publisher ID: ca-pub-5215030892730474 -->
 <!-- Customer ID: 4063247438 -->
 
--- partials/googleAds.php
+++ partials/googleAds.php
@@ -1,5 +1,6 @@
 <!-- Google AdSense Script -->
 <!-- Our identity for sweetelitegame.com -->
+<!-- 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   window.onload = function()
@@ -12,13 +13,5 @@
     );
   }  
 </script>
+ -->
 
-<!-- Google Analytics Script (Global site tag (gtag.js)) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115409161-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-115409161-1');//Analytics
-</script>
